21ea9746fb2ced30de0da8237885cd14d559259a
lummelis <lummelis@hawaii.edu>
2019-09-25 08:11:23 -1000

    initial

---
+++ js/index.js
@@ -0,0 +1,77 @@
+var animation1 = lottie.loadAnimation({
+  container: document.getElementById('bm1'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'js/data.json'
+ })
+
+var animation2 = lottie.loadAnimation({
+  container: document.getElementById('bm2'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'js/parasaurolophus_hover.json'
+})
+
+var animation3 = lottie.loadAnimation({
+  container: document.getElementById('bm3'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'js/images/argentine.json'
+})
+
+var animation4 = lottie.loadAnimation({
+  container: document.getElementById('bm4'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'js/images/argentine_hover.json'
+})
+var animation5 = lottie.loadAnimation({
+  container: document.getElementById('bm5'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'js/images/images/steggie.json'
+})
+var animation6 = lottie.loadAnimation({
+  container: document.getElementById('bm6'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'js/images/images/steggie_hover.json'
+})
+
+var animation7 = lottie.loadAnimation({
+  container: document.getElementById('bm7'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'js/images/images/images/trex.json'
+})
+
+var animation8 = lottie.loadAnimation({
+  container: document.getElementById('bm8'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'js/images/images/images/trex_hover.json'
+})
+
+var animation9 = lottie.loadAnimation({
+  container: document.getElementById('bm9'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'js/images/images/images/images/tri.json'
+})
+
+var animation10 = lottie.loadAnimation({
+  container: document.getElementById('bm10'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'js/images/images/images/images/tri_hover.json'
+})
